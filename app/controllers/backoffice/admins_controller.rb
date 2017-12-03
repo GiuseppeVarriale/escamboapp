63a1ecd1e3bf6d2f35491ae093172b31116aed5d
Giuseppe Varriale <pepe.saoleo@gmail.com>
2017-12-03 16:35:07 +0000

    aprendendo a usar o git

---
+++ app/controllers/backoffice/admins_controller.rb
@@ -0,0 +1,60 @@
+class Backoffice::AdminsController < BackofficeController
+  before_action :set_admin, only: [:edit, :update, :destroy]
+
+  def index
+    @admins = Admin.all
+  end
+
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params_admin)
+    if @admin.save
+      redirect_to backoffice_admins_path, notice: "O Administrador (#{@admin.email}) foi cadastrado com sucesso!"
+    else
+        render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    pwd = params[:admin][:password]
+    pwd_confirmation = params[:admin][:password_confirmation]
+
+    if pwd.blank? && pwd_confirmation.blank?
+      params[:admin].delete(:password)
+      params[:admin].delete(:password_confirmation)
+    end
+
+    if @admin.update(params_admin)
+      redirect_to backoffice_admins_path, notice: "O Administrador (#{@admin.email}) foi atualizado com sucesso!"
+    else
+        render :edit
+    end
+  end
+
+  def destroy
+    admin_email = @admin.email
+
+    if @admin.destroy
+      redirect_to backoffice_admins_path, notice: "O Administrador (#{admin_email}) foi apagado com sucesso!"
+    else
+      render :index
+    end
+  end
+
+  private
+
+    def set_admin
+      @admin = Admin.find(params[:id])
+    end
+
+    def params_admin
+      params.require(:admin).permit(:email, :password, :password_confirmation)
+    end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,33 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Cadastrando as CATEGORIAS..."
+categories= ["Animais e acessórios",
+               "Esportes",
+               "Para a sua casa",
+               "Eletrônicos e celulares",
+               "Música e hobbies",
+               "Bebês e crianças",
+               "Moda e beleza",
+               "Veículos e barcos",
+               "Imóveis",
+               "Empregos e negócios"]
+
+categories.each do | category|
+  Category.find_or_create_by(description: category)
+end
+
+puts "CATEGORIAS Cadastradas com sucesso!"
+######################################################
+
+puts "Cadastrando o Administrador Padrão..."
+
+Admin.create!(email: "admin@admin.com",
+          password: "123456",
+          password_confirmation: "123456")
+
+puts "ADMINISTRADOR  Cadastrado com sucesso!"
